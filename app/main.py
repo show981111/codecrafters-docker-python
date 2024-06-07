417d0a9c72d88b54a6d0d9d0d4ee31303b786021
Yong Seung Lee <leeyongs@umich.edu>
2024-06-07 16:10:47 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ def create_dir_and_copy(dir_name: str) -> Path:
 
     dependency_dir = Path("/usr/local/bin")
     dependency_dir.mkdir(parents=True, exist_ok=True)
-    # shutil.copy2(dependency_dir / "docker-explorer", dependency_dir)
+    shutil.copy2(dependency_dir / "docker-explorer", dependency_dir)
 
     return p
 
